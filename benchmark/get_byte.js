d9d07311dbde53a5207bc6efa662b32494f4c3df
fengmk2 <fengmk2@gmail.com>
2017-05-11 18:38:05 +0800

    test: prototype 继承一样快

--- benchmark/get_byte.js
+++ benchmark/get_byte.js
@@ -1,8 +1,43 @@
 'use strict';
 
-const Benchmark = require('benchmark');
-const benchmarks = require('beautify-benchmark');
-const ByteBuffer = require('../');
+var util = require('util');
+var Benchmark = require('benchmark');
+var benchmarks = require('beautify-benchmark');
+var ByteBuffer = require('..');
+var CopyOldByteBuffer = require('../lib/byte_old');
+
+function OldOldByteBuffer(options) {
+  ByteBuffer.call(this, options);
+}
+util.inherits(OldOldByteBuffer, ByteBuffer);
+
+OldOldByteBuffer.prototype.get = function (index, length) {
+  if (index instanceof Buffer || Array.isArray(index)) { // get (byte[] dst, int offset, int length)
+    var dst = index;
+    var offset = length || 0;
+    length = dst.length;
+
+    this.checkArraySize(dst.length, offset, length);
+    this.checkForUnderflow(length);
+
+    if (dst instanceof Buffer) {
+      this._bytes.copy(dst, offset, this._offset, (this._offset + length));
+    } else {
+      for (var i = offset; i < (offset + length); i++) {
+        dst[i] = this._bytes[i];
+      }
+    }
+    this._offset += length;
+    return this;
+  } else if (typeof index !== 'number') {
+    index = this._offset++;
+  } else if (typeof length === 'number') {
+    // offset, length => Buffer
+    return this._copy(index, index + length);
+  }
+  // return byte
+  return this._bytes[index];
+};
 
 class OldByteBuffer extends ByteBuffer {
   get(index, length) {
@@ -34,27 +69,35 @@ class OldByteBuffer extends ByteBuffer {
   }
 }
 
-const suite = new Benchmark.Suite();
+var suite = new Benchmark.Suite();
 
-const buf = new Buffer([2, 1, 255, 255, 255, 254, 1]);
+var buf = new Buffer([2, 1, 255, 255, 255, 254, 1]);
 
 suite
-  .add('old get()', function() {
-    const bytes = new OldByteBuffer({ array: buf });
+  .add('prototype old get()', function() {
+    var bytes = new OldOldByteBuffer({ array: buf });
+    bytes.get();
+  })
+  .add('copy old get()', function() {
+    var bytes = new OldOldByteBuffer({ array: buf });
+    bytes.get();
+  })
+  .add('class old get()', function() {
+    var bytes = new OldByteBuffer({ array: buf });
     bytes.get();
   })
   .add('new get()', function() {
-    const bytes = new ByteBuffer({ array: buf });
+    var bytes = new ByteBuffer({ array: buf });
     bytes.get();
   })
-  .add('old get(index)', function() {
-    const bytes = new OldByteBuffer({ array: buf });
-    bytes.get(0);
-  })
-  .add('new get(index)', function() {
-    const bytes = new ByteBuffer({ array: buf });
-    bytes.get(0);
-  })
+  // .add('old get(index)', function() {
+  //   var bytes = new OldByteBuffer({ array: buf });
+  //   bytes.get(0);
+  // })
+  // .add('new get(index)', function() {
+  //   var bytes = new ByteBuffer({ array: buf });
+  //   bytes.get(0);
+  // })
   .on('cycle', function(event) {
     benchmarks.add(event.target);
   })
